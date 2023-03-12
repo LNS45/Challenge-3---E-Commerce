92922c82fbb6cd8d3636aed189bc4980f1a1d8f1
Lorenzo <lorenzochavezxd@gmail.com>
2023-03-11 21:06:55 -0600

    Validacion del formulario completada

--- js/index-controller.js
+++ js/index-controller.js
@@ -1,31 +1,31 @@
-
 const $input = document.querySelectorAll("[data-input]");
 
 $input.forEach(element => {
     console.log(element)
     element.addEventListener('blur', element => {
+        element.preventDefault();
         const hijo = element.target;
         const padre = hijo.parentElement;
         const spanError = padre.lastElementChild;
-        validar(hijo,spanError);
+        validar(hijo,spanError,padre);
     });
 });
 
-function validar(hijo,spanError){
+function validar(hijo,spanError,padre){
     const tipoInput = hijo.dataset.input;
+    const iInvalid = padre.querySelector(".contacto__checkInvalid");
+    const iValid = padre.querySelector(".contacto__checkValid");
     if(!hijo.validity.valid){
         spanError.classList.add("contacto__invalid--active");
         spanError.innerHTML = MensajeDeError(tipoInput, hijo);
-
-        if(tipoInput == "email"){
-            const label = document.querySelector(".label-error");
-            console.log(label)
-            label.classList.toggle("label__mismatch");
-        }
+        iInvalid.classList.add('contacto__checkInvalid--active');
+        iValid.classList.remove('contacto__checkValid--active');
 
     }
     else{
         spanError.classList.remove("contacto__invalid--active");
+        iValid.classList.add('contacto__checkValid--active');
+        iInvalid.classList.remove('contacto__checkInvalid--active');
     }
 
 };
