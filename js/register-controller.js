a364fe45a7410a67097bdeb965ffd582e8243359
Lorenzo <lorenzochavezxd@gmail.com>
2023-03-20 00:24:04 -0600

    Inicio de logica para agregar productos

--- js/register-controller.js
+++ js/register-controller.js
@@ -1,3 +1,31 @@
 import { validarFormulario } from "./form-controller.js";
 
 validarFormulario();
+
+
+
+const agregarProducto = (categoria,nombre,precio,descripcion,imagen) => {
+    const URL =  "http://localhost:3000/productos";
+    let objeto = {
+        id: uuid.v4(),
+        nombre: nombre,
+        precio: precio,
+        descripcion: descripcion,
+        categoria: categoria,
+        imagen: imagen
+    };
+    fetch(URL, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(objeto)
+    });
+}
+
+const btn = document.querySelector(".tabla__boton");
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    agregarProducto("Guitarras","Ejemplo","999.99","XD","/saas");
+})
