6d95b3c528e7d7af5c5e87cd020ac3f79210fad8
Lorenzo <lorenzochavezxd@gmail.com>
2023-03-20 22:06:58 -0600

    Logica para agregar productos terminada

--- js/register-controller.js
+++ js/register-controller.js
@@ -2,14 +2,21 @@ import { validarFormulario } from "./form-controller.js";
 
 validarFormulario();
 
-
+function activarBoton(){
+    const btn = document.querySelector(".tabla__boton");
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        const campos = capturarCampos();
+        agregarProducto(campos[1], campos[2],campos[3],campos[4],campos[0]);
+    });
+};
 
 const agregarProducto = (categoria,nombre,precio,descripcion,imagen) => {
     const URL =  "http://localhost:3000/productos";
-    let objeto = {
+    let plantilla = {
         id: uuid.v4(),
         nombre: nombre,
-        precio: precio,
+        precio: `$${precio}`,
         descripcion: descripcion,
         categoria: categoria,
         imagen: imagen
@@ -19,13 +26,20 @@ const agregarProducto = (categoria,nombre,precio,descripcion,imagen) => {
         headers: {
             "Content-type": "application/json"
         },
-        body: JSON.stringify(objeto)
+        body: JSON.stringify(plantilla)
     });
-}
+};
 
-const btn = document.querySelector(".tabla__boton");
+const capturarCampos = () =>{
+    let valoresDeCampos = [];
+    const $campos = document.querySelectorAll("[data-input]");
+    $campos.forEach(campo => {
+        if(campo.dataset.input.includes("Producto")){
+            valoresDeCampos.push(campo.value);
+        };
+    });
+    return valoresDeCampos;
+};
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    agregarProducto("Guitarras","Ejemplo","999.99","XD","/aaass");
-})
+activarBoton();
+
